3b51e37b2373b07246d48b58a0a6f9c7e512a885
unknowntpo <e850506@gmail.com>
2023-04-06 20:25:49 +0800

    implement usecase layer
    FIXME: calling GetPage on the last element in list will receive not
    found error.

--- page/usecase/usecase.go
+++ page/usecase/usecase.go
@@ -12,7 +12,7 @@ type pageUsecaseImpl struct {
 }
 
 func NewPageUsecase(r domain.PageRepo) domain.PageUsecase {
-  return &pageUsecaseImpl{repo: r}
+	return &pageUsecaseImpl{repo: r}
 }
 
 func (u *pageUsecaseImpl) NewList(ctx context.Context, userID int64, listKey domain.ListKey) error {
@@ -20,48 +20,48 @@ func (u *pageUsecaseImpl) NewList(ctx context.Context, userID int64, listKey dom
 		switch {
 		case errors.KindIs(err, errors.ResourceAlreadyExist):
 			return errors.Wrap(errors.ResourceAlreadyExist, "resource already exist", err)
-    default:
-      return errors.Wrap(errors.Internal, "failed on u.repo.NewList", err)
+		default:
+			return errors.Wrap(errors.Internal, "failed on u.repo.NewList", err)
 		}
 	}
 	return nil
 }
 
 func (u *pageUsecaseImpl) GetPage(ctx context.Context, pageKey domain.PageKey) (domain.Page, error) {
-  p, err := u.repo.GetPage(ctx, pageKey)
-  if err != nil {
+	p, err := u.repo.GetPage(ctx, pageKey)
+	if err != nil {
 		switch {
 		case errors.KindIs(err, errors.ResourceNotFound):
 			return domain.Page{}, errors.Wrap(errors.ResourceNotFound, "resource not found", err)
-    default:
-      return domain.Page{}, errors.Wrap(errors.Internal, "failed on u.repo.GetPage", err)
+		default:
+			return domain.Page{}, errors.Wrap(errors.Internal, "failed on u.repo.GetPage", err)
 		}
 	}
-  return p, nil
+	return p, nil
 }
 
 func (u *pageUsecaseImpl) GetHead(ctx context.Context, userID int64, listKey domain.ListKey) (domain.PageKey, error) {
-  headKey, err := u.repo.GetHead(ctx, userID, listKey)
-  if err != nil {
+	headKey, err := u.repo.GetHead(ctx, userID, listKey)
+	if err != nil {
 		switch {
 		// case errors.KindIs(err, errors.ResourceNotFound):
-    //	return errors.Wrap(errors.ResourceNotFound, "resource not found", err)
-    default:
-      return "", errors.Wrap(errors.Internal, "failed on u.repo.GetHead", err)
+		//	return errors.Wrap(errors.ResourceNotFound, "resource not found", err)
+		default:
+			return "", errors.Wrap(errors.Internal, "failed on u.repo.GetHead", err)
 		}
 	}
-  return headKey, nil
+	return headKey, nil
 }
 
 func (u *pageUsecaseImpl) SetPage(ctx context.Context, userID int64, listKey domain.ListKey, page domain.Page) (domain.PageKey, error) {
-  pageKey, err := u.repo.SetPage(ctx, userID, listKey, page)
-  if err != nil {
+	pageKey, err := u.repo.SetPage(ctx, userID, listKey, page)
+	if err != nil {
 		switch {
-		 case errors.KindIs(err, errors.ResourceNotFound):
-    	return "", errors.Wrap(errors.ResourceNotFound, "resource not found", err)
-    default:
-      return "", errors.Wrap(errors.Internal, "failed on u.repo.SetPage", err)
+		case errors.KindIs(err, errors.ResourceNotFound):
+			return "", errors.Wrap(errors.ResourceNotFound, "resource not found", err)
+		default:
+			return "", errors.Wrap(errors.Internal, "failed on u.repo.SetPage", err)
 		}
 	}
-  return pageKey, nil
+	return pageKey, nil
 }
